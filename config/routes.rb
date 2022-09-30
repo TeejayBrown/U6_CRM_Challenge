b3ad1c482af810052f1c57b3a958f13f5d575cf2
TeejayBrown <taiwo.omoleye@outlook.com>
2022-09-29 21:15:07 -0500

    Added URL routes for the Index, Alphabetized, and the Mising_Email Pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
+  get 'customers/alphabetized'
+
+  get 'customers/missing_email'
+
+  #resources :customers, only: [:index]
+
+  root to: 'customers#index'
+  #devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
